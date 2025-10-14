d8cf42aa3facf71e6a455b99a38a36da83afbcbd
Fathima Mazeena Cader <fathimamazeena.mycloudcubicle@gmail.com>
2025-10-14 19:41:05 +0530

    Version 1.1: Error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 // import Projects from '../src/sections/projects/Projects'
-import Skills from '../src/sections/skills/Skills'
+// import Skills from '../src/sections/skills/Skills'
 
 // import Hero from '../src/sections/hero/Hero'
-import Certification from '../src/sections/certifications/Certifications'
-import Interests from '../src/sections/interests/Interests'
-import Footer from '../src/sections/footer/Footer'
-import Contact from '../src/sections/contact/Contact'
-import Navbar from '../src/sections/navbar/Navbar'
-import SplashScreen from '../src/sections/splashScreen/SplashScreen'
+// import Certification from '../src/sections/certifications/Certifications'
+// import Interests from '../src/sections/interests/Interests'
+// import Footer from '../src/sections/footer/Footer'
+// import Contact from '../src/sections/contact/Contact'
+// import Navbar from '../src/sections/navbar/Navbar'
+// import SplashScreen from '../src/sections/splashScreen/SplashScreen'
 import Hero from '../src/sections/hero/Hero'
 
 function App() {
