580fa5721fcf3ee6bf5c2e882972b5aa9a224604
Fathima Mazeena Cader <fathimamazeena.mycloudcubicle@gmail.com>
2025-10-14 18:13:13 +0530

    Version 1: Responsive Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+// import Projects from '../src/sections/projects/Projects'
+import Skills from '../src/sections/skills/Skills'
+
+// import Hero from '../src/sections/hero/Hero'
+import Certification from '../src/sections/certifications/Certifications'
+import Interests from '../src/sections/interests/Interests'
+import Footer from '../src/sections/footer/Footer'
+import Contact from '../src/sections/contact/Contact'
+import Navbar from '../src/sections/navbar/Navbar'
+import SplashScreen from '../src/sections/splashScreen/SplashScreen'
+import Hero from '../src/sections/hero/Hero'
+
+function App() {
+
+
+  return (
+    <>
+    <Hero/>
+    {/* <Projects/>
+     <Skills/> */}
+     {/* <SplashScreen /> */}
+     {/* <Navbar/> */}
+      {/* <Hero/> */}
+      {/* <Certification/>
+      <Skills/>
+      <Interests/>
+      <Contact/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
